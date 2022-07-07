0f8662a0cc185e8891b3c0a5882e9add9c08cb73
Chaitanya Bhilange <107292155+ChaitanyaBhilange@users.noreply.github.com>
2022-07-06 22:56:54 -0700

    Day 3
    Numbers and type conversion

---
+++ type conversion.py
@@ -0,0 +1,80 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+====================== RESTART: D:/Python Practice/text.py =====================
+2597245682192
+<class 'int'>
+
+====================== RESTART: D:/Python Practice/text.py =====================
+2317125616144
+<class 'int'>
+0b1010
+
+====================== RESTART: D:/Python Practice/text.py =====================
+2176062652944
+<class 'int'>
+0b1010
+7
+
+====================== RESTART: D:/Python Practice/text.py =====================
+1671390429712
+<class 'int'>
+0b1010
+7
+0o12
+
+====================== RESTART: D:/Python Practice/text.py =====================
+2026912743952
+<class 'int'>
+0b1010
+7
+0o12
+46
+
+====================== RESTART: D:/Python Practice/text.py =====================
+2641570693648
+<class 'int'>
+0b1010
+7
+0o12
+46
+0xa
+
+====================== RESTART: D:/Python Practice/text.py =====================
+2716896199184
+<class 'int'>
+0b1010
+7
+0o12
+46
+0xa
+Traceback (most recent call last):
+  File "D:/Python Practice/text.py", line 32, in <module>
+    print(int(ox32))
+NameError: name 'ox32' is not defined
+
+====================== RESTART: D:/Python Practice/text.py =====================
+2261567865360
+<class 'int'>
+0b1010
+7
+0o12
+46
+0xa
+50
+s
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    s
+NameError: name 's' is not defined
+
+====================== RESTART: D:/Python Practice/text.py =====================
+2145092436496
+<class 'int'>
+0b1010
+7
+0o12
+46
+0xa
+50
+10.0
