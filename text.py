0d23b5fd09625ed7b8a2c7e71f3bec46c8d98c92
Chaitanya Bhilange <107292155+ChaitanyaBhilange@users.noreply.github.com>
2022-07-06 23:03:31 -0700

    Add files via upload

---
+++ text.py
@@ -0,0 +1,53 @@
+#Type conversion
+
+a=10
+
+#Check momory location
+
+print(id(a))
+
+#To check date type
+print(type(a))
+
+#integer to binary
+print(bin(a))
+
+#binary to integer
+print(int(0b0111))
+
+#.........#
+
+#integer to octal
+print(oct(a))
+
+#octal to integer
+print(int(0o56))
+
+#.........#
+
+#print integer to hexadecimal
+print(hex(a))
+
+#hex to int
+print(int(0x32))
+
+#integer to float
+print(float(a))
+
+#integer to complax
+print(complex(a))
+
+#to check real image part
+num1=5+3j
+print(num1.real)
+print(num1.imag)
+
+
+
+
+
+
+
+
+
+
